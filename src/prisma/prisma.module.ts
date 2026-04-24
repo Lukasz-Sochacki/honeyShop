a7c7d9dc459ce6505463f243464d4b261a6b800d
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-04-24 08:49:31 +0200

    Upload a new structure

--- src/prisma/prisma.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService], // To pozwoli innym modułom używać Prismy
-})
-export class PrismaModule {}
